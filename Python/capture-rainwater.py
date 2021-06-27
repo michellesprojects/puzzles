b2d10817f0e1da4303d849ac9d93559f62326384
Michelle <69691406+michellesprojects@users.noreply.github.com>
2021-06-27 16:19:58 -0400

    update

--- Python/capture-rainwater.py
+++ Python/capture-rainwater.py
@@ -50,3 +50,48 @@ def rain_water(histogram):
         total_water += fill_mark
 
     return total_water
+
+
+
+####### OPTIMIZED SOLUTION HERE
+def optmized_rain_water(histogram):
+
+    #return value
+    total_water = 0 
+
+    
+    left_maxes = []
+    right_maxes = []
+    
+    left_max = histogram[0]
+
+    #loop through all values and get the max left value relative to i, append it to left_maxes
+    for i in range(0, len(histogram)):
+
+      if(histogram[i] > left_max):
+        left_max = histogram[i]
+
+      left_maxes.append(left_max)
+  
+    right_max = histogram[-1]
+
+    #loop through all values and get the max right value relative to i, append it to right_maxes
+    for i in range(len(histogram)-1,-1 ,-1):
+      if(histogram[i] > right_max):
+        right_max = histogram[i]
+      right_maxes.append(right_max)
+
+    #reverse right_maxes so indices align with left_maxes
+    right_maxes.reverse()
+  
+    #loop through values, find 2nd largest of two maxes and add the remainer of it - value at i to total_water
+    for i in range(1, len(histogram)-1):
+      val = min(right_maxes[i],left_maxes[i]);
+      total_water += val - histogram[i]
+
+    return total_water
+
+####### BENCHMARKING HERE
+print(optmized_rain_water(small))
+print(optmized_rain_water(medium))
+print(optmized_rain_water(edge_case))
